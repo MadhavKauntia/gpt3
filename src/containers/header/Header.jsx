04bbe6f07afe29c2deb9f9b433ad4ac4ae030706
Madhav Kauntia <mkauntia@gmail.com>
2022-01-30 19:53:00 +0530

    Set up Navbar

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="gpt3__header section__padding">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
